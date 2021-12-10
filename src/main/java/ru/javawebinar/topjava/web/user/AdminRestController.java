6ec253388ea5be34530db2578f15bb16048b63bf
Artem.Vogulev <Artem.Vogulev@russianpost.ru>
2021-12-10 15:57:38 +0300

    Apply 8_* patches

--- src/main/java/ru/javawebinar/topjava/web/user/AdminRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminRestController.java
@@ -56,4 +56,16 @@ public class AdminRestController extends AbstractUserController {
     public User getByMail(@RequestParam String email) {
         return super.getByMail(email);
     }
+
+    @GetMapping("/{id}/with-meals")
+    public User getWithMeals(@PathVariable int id) {
+        return super.getWithMeals(id);
+    }
+
+    @Override
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void enable(@PathVariable int id, @RequestParam boolean enabled) {
+        super.enable(id, enabled);
+    }
 }
